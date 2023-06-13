27acb6c4ca5f306adb0b2ef053955bbff87d9e3c
Andrew <a.matrosov@colynn.pro>
2023-06-13 17:48:46 +0300

    add 2 versions of animation

---
+++ v2.1/js/numbers.js
@@ -0,0 +1,25 @@
+const numbersList = [
+	{
+		"category": "premium",
+		"price": 79.9,
+		"number": "+7 (926) 96-000-18"
+	},
+	{
+		"category": "combo x4",
+		"price": 79.9,
+		"number": "+7 (926) 96-000-11"
+	},
+	{
+		"category": "combo x3",
+		"price": 79.9,
+		"number": "+7 (926) 111-00-00"
+	},
+	{
+		"category": "double",
+		"price": 59.9,
+		"number": "+7 (926) 111-01-11"
+	}
+
+]
+
+export default numbersList;
